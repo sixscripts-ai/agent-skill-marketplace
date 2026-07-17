a71530ad33ebd3f03b15d38bc7d2cdccbdf85f24
Ashton Aschenbrener <aaschenbrener8978@student.miracosta.edu>
2026-07-17 14:47:45 -0700

    feat(builder): add shared builder types

---
+++ src/components/builder/builder-types.ts
@@ -0,0 +1,11 @@
+export type BuilderVisibility = "public" | "private" | "unlisted";
+
+export type BuilderViewMode = "markdown" | "canvas";
+
+export type BuilderSavedUrls = {
+  detail: string;
+  marketplace: string;
+  mySkills: string;
+  run: string;
+  edit: string;
+};
